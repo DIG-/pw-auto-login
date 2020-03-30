d7018cfacc06dbbebf548546fb4c8fbf2ca503e7
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2020-03-30 12:28:56 -0300

    Models to hold information

---
+++ hdr/data/login.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <optional>
+
+#include "data/server.hpp"
+
+namespace DIG {
+namespace Data {
+
+typedef struct Login {
+  std::string username;
+  std::string password;
+  std::optional<Server> server;
+} Login;
+
+}  // namespace Data
+}  // namespace DIG
---
+++ hdr/data/server.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+
+namespace DIG {
+namespace Data {
+
+typedef struct Server {
+  std::string name;
+  std::string address;
+} Server;
+
+}  // namespace Data
+}  // namespace DIG
