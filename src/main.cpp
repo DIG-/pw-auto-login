faf376d453850e2003cda33a49ed598ab1accf9c
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-04-30 20:45:36 -0300

    Started main

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 
+#include "config/config.hpp"
+#include "crypto.hpp"
+#include "data/account.hpp"
 #include "error.hpp"
+#include "os.hpp"
+#include "ui.hpp"
+
+#include <cxxopts.hpp>
 
 int main(int argc, char** argv) {
-  std::cout << "Hello World!" << std::endl;
-  DIG::Err a = DIG::Err::OK;
+  cxxopts::Options options("PW-Auto-Login",
+                           "Simple creative Perfect World launcher");
+  // clang-format off
+  options.add_options()
+          ("config-file","Config file (optional)",cxxopts::value<std::string>(),"FILE")
+          ("s,account-store","Index of account-store in config file",cxxopts::value<int>(),"INDEX")
+          ("account-store-file","Account-store file to be used",cxxopts::value<std::string>(),"FILE")
+          ("a,account","Index of account inside account-store to be logged",cxxopts::value<int>(),"INDEX")
+          ;
+  // clang-format on
+
+  auto result = options.parse(argc, argv);
+  if (result.count("account") > 1) {
+    DIG::UI::show_error_message("Only one account per shortcut");
+    return 1;
+  } else if (result.count("account") == 1) {
+    // TODO: Login
+    return 0;
+  }
+
+  std::cout << "Initialize UI" << std::endl;
   return 0;
 }
