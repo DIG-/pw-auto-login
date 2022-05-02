7eeb2907ea6e80f80c3668e2488f578c3ec20673
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-01 22:28:45 -0300

    Creating menu

--- src/ui/main2.cpp
+++ src/ui/main2.cpp
@@ -6,14 +6,28 @@ namespace Main2 {
 
 #include "main.c"
 
+constexpr auto MENU = "DIG::UI::Main::Menu";
+constexpr auto SETTINGS_ACTION = "DIG::UI::Main::Settings";
+
 Ihandle* account_list;
 Ihandle* account_add;
 Ihandle* account_edit;
 Ihandle* account_rem;
 Ihandle* account_launch;
 
+void create_menu() {
+  // clang-format off
+  auto menu = IupMenu(
+    IupItem("Settings", SETTINGS_ACTION),
+    nullptr
+  );
+  // clang-format on
+  IupSetHandle(MENU, menu);
+}
+
 void create() {
   auto dialog = IupDialog(create_layout());
+  create_menu();
   account_list = IupGetHandle("account_list");
   account_add = IupGetHandle("account_add");
   account_edit = IupGetHandle("account_edit");
@@ -22,6 +36,7 @@ void create() {
 
   IupSetAttribute(dialog, "TITLE", "PW Auto Login");
   IupSetAttribute(dialog, "RESIZE", "NO");
+  IupSetAttribute(dialog, "MENU", MENU);
 
   IupSetAttribute(account_edit, "ACTIVE", "NO");
   IupSetAttribute(account_rem, "ACTIVE", "NO");
