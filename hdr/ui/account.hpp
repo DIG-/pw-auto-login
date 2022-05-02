8723ff6ea1bf141d9306a024679adbd7d50534d1
Gustavo de Oliveira Silva (DIG) <diguser@gmail.com>
2022-05-01 23:00:07 -0300

    Initial structure to account screen

---
+++ hdr/ui/account.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <cstdint>
+
+namespace DIG {
+namespace UI {
+namespace Account {
+
+bool add_account();
+bool edit_account(const uint_fast16_t&);
+
+}  // namespace Account
+}  // namespace UI
+}  // namespace DIG
---
+++ src/ui/account.cpp
@@ -0,0 +1,17 @@
+#include "ui/account.hpp"
+
+#include <iup.h>
+namespace DIG {
+namespace UI {
+namespace Account {
+
+bool add_account() {
+  return false;
+}
+bool edit_account(const uint_fast16_t&) {
+  return false;
+}
+
+}  // namespace Account
+}  // namespace UI
+}  // namespace DIG
